d4f404a480b079d1ec4698154afbcdca9db0a92e
mi <clock2elizabeth@gmail.com>
2019-05-04 16:12:53 +0900

    add socket.io, update input & show page

--- honne/routes/input.js
+++ honne/routes/input.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET処理 入力フォームの表示 */
 router.get('/', function(req, res, next) {
-  res.render('input');
+  var data = {
+    pagetitle: '本音を教えて！',
+    comments: 'これが私の本心だよ。',
+    classname: 'clean',
+    url: '/input/post',
+    msg: 'キレイにする！'
+  }
+  res.render('input', data);
+});
+
+/* POST処理 結果ページの表示 */
+router.post('/post', function(req, res, next) {
+  var data = {
+    pagetitle: 'キレイになったよ！',
+    comments: '結果',
+    classname: 'submit',
+    url: '/show',
+    msg: '本音を言う'
+  }
+  res.render('input', data);
 });
 
 module.exports = router;
--- honne/routes/show.js
+++ honne/routes/show.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET処理 */
 router.get('/', function(req, res, next) {
+  res.render('show');
+});
+
+/* POST処理 */
+router.post('/', function(req, res, next) {
   res.render('show');
 });
 
